8f939c479c5767e1f787fa7e8dfd3dde5178ff63
Bastian Kroggel <b.kroggel@yahoo.de>
2018-12-05 17:46:31 +0100

    added introduction, flashback, powered_by, multiple changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,6 +4,8 @@ console.log('STARTUP AUTOBAHN powered by Plug and Play\nEXPO Day 5 on February 2
 
 {% include js/countdown.min.js %}
 {% include js/jquery.slabtext.min.js %}
+{% include js/masonry.min.js %}
+{% include js/imagesloaded.min.js %}
 
 $('#expo_countdown p').countdown('2019/02/21 14:00:00', function(event) {
   $(this).html(event.strftime('%T'));
@@ -21,3 +23,132 @@ $('.placeholder__inner').click( function( e ){
   }
   $(this).toggleClass('active');
 })
+
+// $('.read_more').each( function(){
+//   $(this).click( function(){
+//     console.log('works')
+//   })
+// })
+
+function read_more() {
+  $('.speaker__item').each( function() {
+    var section = $(this).find('.speaker__info');
+    var read_more = $(this).find('.read_more');
+    var desc_padding = $(section).find('.desc').outerHeight() - $(section).find('.desc').height()
+    var sub_el = $(read_more).height() - desc_padding;
+    var content = $(section)[0].scrollHeight - sub_el;
+    var space = $(section).outerHeight();
+    if (content > space && !$(read_more).hasClass('true') ) {
+      $(read_more).addClass('true');
+    } else if ( (content < space && $(read_more).hasClass('true') ) ) {
+      $(read_more).removeClass('true');
+    }
+  })
+}
+
+read_more();
+
+$('.read_more').click( function(){
+  var main = $(this).parents('.speaker__item');
+  var parent = $(this).parents('.speaker__info');
+  if ( !$(main).hasClass('active') ) {
+    var height = $(parent)[0].scrollHeight + 40 + 40;
+    $(main).addClass('active').css('height', height);
+  } else {
+    $(main).removeClass('active').css('height', 200);
+  }
+})
+
+function reset_speaker_items() {
+  $('.speaker__item').each( function(){
+    $(this).removeClass('active').css('height', 200);
+  });
+  setTimeout(function () {
+    read_more();
+  }, 600);
+}
+
+// flashback masonry
+var $flashback = $('.flashback__grid').masonry({
+  itemSelector: '.flashback__item',
+  percentPosition: true
+});
+
+$flashback.imagesLoaded().progress( function() {
+  $flashback.masonry('layout');
+});
+
+// calc height of masonry
+var masonry_height = 0;
+
+function init_height_masonry() {
+  var grid = $('.flashback__grid').outerHeight(true);
+  var main = $('.flashback')
+  var expand_bar = main.find('.expand_bar').outerHeight(true);
+  var header = main.find('h2').outerHeight(true) + main.find('p.intro').outerHeight(true);
+  if (grid > 1200) {
+    masonry_height = header + 1200;
+    main.find('.expand_bar').css('display', '');
+  } else {
+    masonry_height = grid + header + expand_bar;
+    main.find('.expand_bar').css('display', 'none');
+  }
+  main.css('height', masonry_height);
+}
+
+init_height_masonry();
+
+// expand functions
+
+function expand_bar_open(obj) {
+  var height = obj[0].scrollHeight + obj.find('.expand_bar').outerHeight();
+  $(obj).css('height', height).addClass('active');
+}
+
+function expand_bar_close(obj) {
+  $(obj).css('height', masonry_height).removeClass('active');
+  $('html, body').animate({
+    scrollTop: ( $(obj).offset().top )
+  }, 600);
+
+}
+
+function expand_bar() {
+  var main = $('.flashback')
+  if ( $(main).hasClass('active') ) {
+    expand_bar_close(main);
+  } else {
+    expand_bar_open(main);
+  }
+}
+
+$('.expand_bar').click( function() {
+  expand_bar();
+})
+
+// Fancybox
+$('.flashback__item a').fancybox({
+   selector : '.flashback__item a',
+   afterShow: function() {
+     var main = $('.flashback')
+     if ( !$(main).hasClass('active') ) {
+       expand_bar_open(main);
+     }
+   },
+   afterClose: function() {
+     var main = $('.flashback')
+     if ( $(main).hasClass('active') ) {
+       expand_bar_close(main);
+     }
+   },
+});
+
+$( window ).resize(function() {
+  reset_speaker_items();
+  read_more();
+  init_height_masonry();
+  var main = $('.flashback')
+  if ( $(main).hasClass('active') ) {
+    expand_bar_close(main);
+  }
+});
